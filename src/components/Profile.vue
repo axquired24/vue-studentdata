a8a7b4f86b34e691da174a0d6480a7524e655fef
Albert Septiawan <albert.septiawan@ansvia.com>
2018-07-23 16:48:11 +0700

    add(selectBox Component)

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,19 +1,32 @@
 <template>
-  <HeaderTitle title="Vue Profile!" description="Describe yourself"></HeaderTitle>
+  <div>
+    <HeaderTitle title="Vue Profile!" description="Describe yourself"></HeaderTitle>
+    <SelectBoxStudent :kelas="kelas"></SelectBoxStudent>
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
+import axios from 'axios'
+import lodash from 'lodash'
 import HeaderTitle from './layouts/HeaderTitle'
+import SelectBoxStudent from './core/SelectBoxStudent'
+
+function KelasM(id, name) {
+  return {id: id, name: name}
+}
 
 export default {
   name: 'Profile',
   components: {
-    HeaderTitle
+    HeaderTitle, SelectBoxStudent
   },
   data () {
     return {
+      kelas: KelasM(6, "NamKelas")
     }
+  },
+  methods: {
   }
 }
 </script>
---
+++ src/components/core/SelectBoxStudent.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+      <span v-if="isLoading">Loading...</span>
+      <SelectBoxBase v-if="! isLoading" :model="model" :opt="opt"></SelectBoxBase>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from 'axios'
+import lodash from 'lodash'
+import SelectBoxBase from '../base/SelectBoxBase'
+
+const baseUrl = "https://api.myjson.com/bins/8vm42";
+
+export default {
+  name: 'SelectBoxStudent',
+  props: {
+      kelas: {
+          type: Object,
+          required: true
+      }
+  },
+  components: {
+    SelectBoxBase
+  },
+  data () {
+    return {
+      dataUrl: baseUrl,
+      isLoading: true,
+      opt: [],
+      model: {}
+    }
+  },
+  watch: {
+      kelas: {
+            handler(newVal, oldVal) {
+                console.log("Changed Val: " + JSON.stringify(newVal));
+                this.loadKelas(newVal.id);
+            },
+            // used to watch all element inside, watch computed value to inspect spesific value
+            // https://stackoverflow.com/questions/42133894/vue-js-how-to-properly-watch-for-nested-data
+            deep: true
+      }
+  },
+  mounted() {
+    this.loadKelas();
+  },
+  methods: {
+    loadKelas: function(kelasId = this.kelas.id) {
+        this.isLoading = true
+        this.dataUrl = baseUrl + "?kelasId=" + kelasId
+        const vm = this
+            axios.get(this.dataUrl)
+            .then(response => {
+                const studentRows = response.data;
+                vm.opt = _.map(studentRows, function(m) {
+                    return {val: m.id, label: m.name}
+                });
+                this.isLoading = false;
+                vm.model = vm.opt[0];
+            }).catch(error => {
+                console.log(error);
+                this.isLoading = false;
+            })
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
